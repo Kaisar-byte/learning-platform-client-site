7bdb76546368d35ce897c98fe6786183e3739598
mohammadkaisarhamid <mohammadkaisarhamid@gmail.com>
2022-11-17 20:31:35 +0600

    started

---
+++ src/component/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import heroImg from '../../assets/images/hero-img.png'
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h2>Start Your Programming Training</h2>
+            </div>
+            <img src={heroImg} alt="" />
+        </div>
+    );
+};
+
+export default Home;
