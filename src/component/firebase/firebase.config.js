206dc994af6a30032d50c5e2689588331192d725
Kaisar-byte <mohammadkaisarhamid@gmail.com>
2022-12-13 23:58:22 +0600

    env variable added

--- src/component/firebase/firebase.config.js
+++ src/component/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyDGL2v7tELCyhqfrI1NoNESE48oGLvIfTo",
-	authDomain: "programming-zone-assignment.firebaseapp.com",
-	projectId: "programming-zone-assignment",
-	storageBucket: "programming-zone-assignment.appspot.com",
-	messagingSenderId: "26363622633",
-	appId: "1:26363622633:web:e4f90a3d9d939165c92c78",
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
